e46fe6ea923f1a562458e6fd1fe23ffffeca3ccc
wangluyuan <545000692@qq.com>
2017-03-22 16:57:29 +0800

    用户重复登录修改

--- routers/auth.js
+++ routers/auth.js
@@ -4,10 +4,9 @@ var userDao = require('../dao/usersDao')
 router.post('/login', function (req, res) {
     var user = req.session.user
     if (user) {
-        // TODO return to index page
-    } else {
-        userDao.userLogin(req, res)
+        req.session.destroy()
     }
+    userDao.userLogin(req, res)
 })
 
 router.get('/logout', function (req, res) {
